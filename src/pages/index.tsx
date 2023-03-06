4e5dc056c3eb8e232b42f73ccc4a7b0cc1f0fdf4
ryota-09 <n.ryo950911@gmail.com>
2023-03-06 14:33:37 +0900

    閉じタグのエラー

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
+import { Blog, getList } from "@/lib/microcms";
 
-export default function Home() {
+import { serialize } from "next-mdx-remote/serialize";
+import { GetServerSideProps } from "next";
+import { MDXRemote } from "next-mdx-remote";
+
+export default function Home({ source }) {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+
+  async function fetchBlogs() {
+    const { contents } = await getList();
+    setBlogs(contents);
+  }
+
+  const getContent = async () => {
+    const mdxSource = await serialize(
+      blogs[0].content
+      // `<h2>ブログテンプレートから作成されました</h2>`
+    );
+  };
+
   return (
     <>
       <Head>
@@ -12,7 +32,18 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1>Next12</h1>
+        <button onClick={fetchBlogs}>フェッチボタン</button>
+        <button onClick={fetchBlogs}>シリアライズボタン</button>
+        {blogs && blogs.map((blog) => <li key={blog.id}>{blog.title}</li>)}
+        <MDXRemote {...source} />
       </main>
     </>
-  )
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { contents } = await getList();
+  // MDX text - can be from a local file, database, anywhere
+  const mdxSource = await serialize(contents[1].content.replaceAll("<br>", ""));
+  return { props: { source: mdxSource } };
+};
